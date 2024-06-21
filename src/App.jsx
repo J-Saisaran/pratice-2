25dc0f1ac1be06804ca89824e3bd9b3b54539ebb
J-Saisaran <saransai027@gmail.com>
2024-06-21 22:43:26 +0530

    Admin dashboard

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+import Sidebar from './Sidebar'
+import Topbar from './Topbar'
+import Dashboard from './Dashboard';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar/>
+        <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+          <Topbar/>
+          <div className="container-fluid">
+            <Switch>
+              <Route exact={true} path="/" component={Dashboard}/>
+            </Switch>
+            
+          </div>
+        </div>
+
+
+
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
